d30b4b8073d0c51b2b9a3bbe67460b1db6b45d76
SanchezYoan <yoan30470@hotmail.fr>
2023-05-11 00:13:29 +0200

    checked

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
+const form = document.querySelector("form");
+const inputs = document.querySelectorAll(
+  'input[type="text"], input[type="password"]'
+);
+const progressBar = document.getElementById("progress-bar");
+let pseudo, email, password, confirmPass;
 
-// Priorités en CSS : id > classe > baliseHtml
+const errorDisplay = (tag, message, valid) => {
+  const container = document.querySelector("." + tag + "-container");
+  const span = document.querySelector("." + tag + "-container > span");
 
-const sidebar = document.querySelector("#side-bar")
-const content = document.querySelector(".content")
+  if (!valid) {
+    container.classList.add("error");
+    span.textContent = message;
+  } else {
+    container.classList.remove("error");
+    span.textContent = message;
+  }
+};
 
-
-btn.addEventListener("click", () => {
-    sidebar.classList.toggle('active');  
-
-})
-
-content.addEventListener("click", () => {
-    sidebar.classList.remove("active")
-})
+const pseudoChecker = (value) => {
+  if (value.length > 0 && (value.length < 3 || value.length > 20)) {
+    errorDisplay("pseudo", "Le pseudo doit faire entre 3 et 20 caractères");
+    pseudo = null;
+  } else if (!value.match(/^[a-zA-Z0-9_.-]*$/)) {
+    errorDisplay(
+      "pseudo",
+      "Le pseudo ne doit pas contenir de caractères spéciaux"
+    );
+    pseudo = null;
+  } else {
+    errorDisplay("pseudo", "", true);
+    pseudo = value;
+  }
+};
