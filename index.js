5340963fac240935c3eee01e6516a7a025dccdbf
SanchezYoan <yoan30470@hotmail.fr>
2023-05-14 20:14:59 +0200

    start end

--- index.js
+++ index.js
@@ -1,35 +1,48 @@
-const form = document.querySelector("form");
-const inputs = document.querySelectorAll(
-  'input[type="text"], input[type="password"]'
-);
-const progressBar = document.getElementById("progress-bar");
-let pseudo, email, password, confirmPass;
+// Convert today day to input format
 
-const errorDisplay = (tag, message, valid) => {
-  const container = document.querySelector("." + tag + "-container");
-  const span = document.querySelector("." + tag + "-container > span");
+const today = new Date().toISOString().split("T")[0];
+start_date.value = today;
+start_date.min = today;
 
-  if (!valid) {
-    container.classList.add("error");
-    span.textContent = message;
-  } else {
-    container.classList.remove("error");
-    span.textContent = message;
+// Tomorow date calc
+let tomorrow = new Date();
+tomorrow.setDate(tomorrow.getDate() + 1);
+
+// Convert to input format
+let tomorrowFormat = tomorrow.toISOString().split("T")[0];
+end_date.value = tomorrowFormat;
+end_date.min = tomorrowFormat;
+
+start_date.addEventListener("change", (e) => {
+  let day = new Date(e.target.value)
+  
+  if (end_date.value < start_date.value){
+    day.setDate(day.getDate() + 1);
+    end_date.value = day.toISOString().split("T")[0];
   }
-};
+})
 
-const pseudoChecker = (value) => {
-  if (value.length > 0 && (value.length < 3 || value.length > 20)) {
-    errorDisplay("pseudo", "Le pseudo doit faire entre 3 et 20 caractères");
-    pseudo = null;
-  } else if (!value.match(/^[a-zA-Z0-9_.-]*$/)) {
-    errorDisplay(
-      "pseudo",
-      "Le pseudo ne doit pas contenir de caractères spéciaux"
+end_date.addEventListener("change", (e) => {
+  let day = new Date(e.target.value)
+
+  if (end_date < start_date) {
+    day.setDate(day.getDate() - 1);
+    start_date.max = day.toISOString().split("T")[0];
+  }
+})
+
+const bookingCalc = () => {
+  // différence entre la date de début et de fin
+  let diffTime = Math.abs(
+    new Date(end_date.value) - new Date(start_date.value)
     );
-    pseudo = null;
-  } else {
-    errorDisplay("pseudo", "", true);
-    pseudo = value;
+    
+    // 1000 milisec * 60sec * 60min * 24h
+    let diffDay = Math.ceil(diffTime / ( 1000 * 60 * 60 * 24 ))
+    total.textContent = diffDay * nightPrice.textContent
+
+
   }
-};
+
+  start_date.addEventListener("change", bookingCalc)
+  end_date.addEventListener("change", bookingCalc)
